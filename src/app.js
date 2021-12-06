0bc962fe9bfad1a64e66dd8abbd6e5ff2adc42f6
mgrbz <mgurbuzy@gmail.com>
2021-12-06 22:33:17 +0300

    base router setup

--- src/app.js
+++ src/app.js
@@ -1,21 +1,45 @@
 
 
 
-import React from 'react'
+import React from 'react';
 import { Provider } from 'react-redux';
 
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link
+  } from "react-router-dom";
 
+
+import Footer from './containers/Footer';
 import Home from './containers/Home'
-
 import store from './utils/store';
 
 const App = () => {
     return (
         <Provider store={store}>
-            <Home />
+            <Footer />
+            <BrowserRouter>
+
+                <Link to="/invoices">Invoices</Link> |{" "}
+                <Link to="/expenses">Expenses</Link>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="expenses" element={<About />} />
+                    <Route path="invoices" element={<Home />} />
+                </Routes>
+            </BrowserRouter>
         </Provider>
     )
 }
 
+const About = () => {
+    return(
+        <div>
+            About
+        </div>
+    )
+}
 
 export default App;
---
+++ src/containers/Footer/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import './footer.scss';
+
+const Footer = (props) => {
+    console.log('Footer props', props)
+    return (
+        <div>
+            Footer
+        </div>
+    )
+}
+
+const mapStateToProps = (state) =>{
+    return(
+        state
+    )
+}
+
+const mapDispatchToProps = {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Footer)
