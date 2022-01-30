010951e0ecbdda0dd30115a1bd84ea1606f5fef8
mgrbz <mgurbuzy@gmail.com>
2022-01-30 13:47:47 +0300

    yeni chartlar eklendi

--- src/components/CReactVis/CReactVis.js
+++ src/components/CReactVis/CReactVis.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 import 'react-vis/dist/style.css';
-import {XYPlot, LineSeries, VerticalBarSeries} from 'react-vis';
+import {XYPlot, LineSeries, VerticalBarSeries, XAxis, YAxis, RadialChart} from 'react-vis';
 
 const CReactVis = () => {
 
@@ -26,131 +26,150 @@ const CReactVis = () => {
                 <LineSeries data={data} />
                 </XYPlot>
 
-                <XYPlot  height={300} width={600}>
+                <XYPlot  
+                    height={300} 
+                    width={600}
+                    colorRange={[
+                        '#e50f63',
+                        '#DDB27C',
+                        '#88572C'
+                    ]}
+                    colorScale="category"
+                >
+                    <XAxis/><YAxis/>
                     <VerticalBarSeries
-                        data={[
-                        {
-                            x: 0,
-                            y: 10
-                        },
-                        {
-                            x: 1,
-                            y: 10.681775986762196
-                        },
-                        {
-                            x: 2,
-                            y: 9.635801336632877
-                        },
-                        {
-                            x: 3,
-                            y: 9.22793399666694
-                        },
-                        {
-                            x: 4,
-                            y: 10.504333029249544
-                        },
-                        {
-                            x: 5,
-                            y: 11.592863069205547
-                        },
-                        {
-                            x: 6,
-                            y: 9.947023915395198
-                        },
-                        {
-                            x: 7,
-                            y: 10.245009550794029
-                        },
-                        {
-                            x: 8,
-                            y: 8.334480103894771
+                        data={
+                            [
+                                {
+                                    x: 0,
+                                    y: 10,
+                                    color: 1,
+                                },
+                                {
+                                    x: 1,
+                                    y: 10,
+                                    color: 1,
+                                },
+                                {
+                                    x: 2,
+                                    y: 11,
+                                    color: 1,
+                                },
+                                {
+                                    x: 3,
+                                    y: 12,
+                                    color: 1,
+                                },
+                                {
+                                    x: 4,
+                                    y: 14,
+                                    color: 1,
+                                },
+                                {
+                                    x: 5,
+                                    y: 15,
+                                    color: 1,
+                                },
+                                {
+                                    x: 6,
+                                    y: 13,
+                                    color: 1,
+                                },
+                                {
+                                    x: 7,
+                                    y: 14,
+                                    color: 1,
+                                },
+                                {
+                                    x: 8,
+                                    y: 13,
+                                    color: 1,
+                                }
+                            ]
                         }
-                        ]}
                         style={{}}
                     />
                     <VerticalBarSeries
                         data={[
                         {
                             x: 0,
-                            y: 10
+                            y: 10,
+                            color: 3,
                         },
                         {
                             x: 1,
-                            y: 10.38669428381387
+                            y: 8.448685934192767,
+                            color: 3,
                         },
                         {
                             x: 2,
-                            y: 11.404285514420597
+                            y: 7.265749540157546,
+                            color: 3,
                         },
                         {
                             x: 3,
-                            y: 12.729691609298733
+                            y: 8.331417285421509,
+                            color: 3,
                         },
                         {
                             x: 4,
-                            y: 13.933558058168769
+                            y: 7.067287203427849,
+                            color: 3,
                         },
                         {
                             x: 5,
-                            y: 14.143310190230983
+                            y: 6.3110979712322095,
+                            color: 3,
                         },
                         {
                             x: 6,
-                            y: 12.83853044612545
+                            y: 7.128270930380114,
+                            color: 3,
                         },
                         {
                             x: 7,
-                            y: 13.983493588886663
+                            y: 6.830334438360218,
+                            color: 3,
                         },
                         {
                             x: 8,
-                            y: 13.031341803189028
-                        }
-                        ]}
-                        style={{}}
-                    />
-                    <VerticalBarSeries
-                        data={[
-                        {
-                            x: 0,
-                            y: 10
-                        },
-                        {
-                            x: 1,
-                            y: 8.448685934192767
-                        },
-                        {
-                            x: 2,
-                            y: 7.265749540157546
-                        },
-                        {
-                            x: 3,
-                            y: 8.331417285421509
-                        },
-                        {
-                            x: 4,
-                            y: 7.067287203427849
-                        },
-                        {
-                            x: 5,
-                            y: 6.3110979712322095
-                        },
-                        {
-                            x: 6,
-                            y: 7.128270930380114
-                        },
-                        {
-                            x: 7,
-                            y: 6.830334438360218
-                        },
-                        {
-                            x: 8,
-                            y: 7.3116262131247876
+                            y: 7.3116262131247876,
+                            color: 3,
                         }
                         ]}
                         style={{}}
                     />
                 </XYPlot>
+
+                <RadialChart  height={300} width={300}
+                    data={[
+                        {
+                        angle: 16,
+                        label: 'deck.gl'
+                        },
+                        {
+                        angle: 17,
+                        label: 'math.gl'
+                        },
+                        {
+                        angle: 27,
+                        label: 'probe.gl'
+                        },
+                        {
+                        angle: 12,
+                        label: 'vis.gl'
+                        },
+                        {
+                        angle: 17,
+                        label: 'react-map-gl'
+                        }
+                    ]}
+                    labelsRadiusMultiplier={1.1}
+                    labelsStyle={{
+                        fontSize: 12
+                    }}
+                    showLabels
+                />
 
             </div>
             
