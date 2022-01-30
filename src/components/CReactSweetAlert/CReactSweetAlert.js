e1f2eaa1392a9adc0e8d960e585772c0afb9fe0f
mgrbz <mgurbuzy@gmail.com>
2022-01-30 16:15:48 +0300

    some changes

--- src/components/CReactSweetAlert/CReactSweetAlert.js
+++ src/components/CReactSweetAlert/CReactSweetAlert.js
@@ -1,25 +1,82 @@
-import React, { useState } from 'react';
-import { withSwalInstance } from 'sweetalert2-react';
+import React, { useEffect, useState } from 'react';
 import swal from 'sweetalert2';
  
 import './reactsweetalert.scss'
-const SweetAlert = withSwalInstance(swal);
  
-
+const Toast = swal.mixin({
+    toast: true,
+    position: 'bottom-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', swal.stopTimer)
+      toast.addEventListener('mouseleave', swal.resumeTimer)
+    }
+  })
+  
 
 const CReactCompareSlider = () => {
 
     const [show, setShow] = useState(false);
 
+    useEffect(() => {
+        if(show){
+
+
+            //Alert1
+            Toast.fire({
+                icon: 'info',
+                title: 'Signed in successfully'
+            })
+
+
+            //Alert2
+
+            // swal.fire({
+            //     template: '#my-template'
+            //   })
+        }
+
+    }, [show])
+
+    const changeShow = () => {
+        setShow(!show)
+
+
+        setTimeout(() => {
+            setShow(false)
+        }, 3000);
+        
+    }
+
+
     return (
         <div className='reactsweetalert_wrapper'>
-            <button onClick={() => setShow(true)}>Alert</button>
-            <SweetAlert
-                show={show}
-                title="Demo"
-                text="SweetAlert in React"
-                onConfirm={() => setShow(false)}
-            />
+            <button onClick={() => changeShow()}>Alert</button>
+
+            {/* <template id="my-template">
+
+                Selam
+                <swal-title>
+                    Save changes to "Untitled 1" before closing?
+                </swal-title>
+                <swal-icon type="warning" color="red"></swal-icon>
+                <swal-button type="confirm">
+                    Save As
+                </swal-button>
+                <swal-button type="cancel">
+                    Cancel
+                </swal-button>
+                <swal-button type="deny">
+                    Close without Saving
+                </swal-button>
+                <swal-param name="allowEscapeKey" value="false" />
+                <swal-param
+                    name="customClass"
+                    value='{ "popup": "my-popup" }' />
+            </template> */}
+
         </div>
 
     );
